63bf8b98317dc65889463b680b3b11e659b7f82d
Shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2022-10-29 16:55:56 +0100

    removed trailing carriage returns

--- stack/compile.sh
+++ stack/compile.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-
 BIN_DIRECTORY="bin"
-
 if [ ! -d $BIN_DIRECTORY ]; then
   mkdir ./bin
 fi
-
 gcc -Wall -c stack.c -o ./bin/stack.o
 gcc -Wall -c main.c -o ./bin/main.o
 gcc -Wall -c infixToPostfix.c -o ./bin/infixToPostfix.o
