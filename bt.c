3784df690a58ed46125814cde918f7e07b86b8c2
shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2022-12-19 11:58:31 +0530

    Update bt.c

--- bt.c
+++ bt.c
@@ -10,12 +10,14 @@ struct _node
     node_ptr right;
 };
 
+node_ptr create();
+void inorder(node_ptr root);
+void preorder(node_ptr root);
+void postorder(node_ptr root);
+void levelorder(node_ptr root);
+void delete(int data);
+int height(node_ptr root);
 
-node_ptr create_node(int data);
-void insert(node_ptr* tree, int data);
-void del_tree(node_ptr tree);
-node_ptr search(node_ptr* tree, int data);
-void print_preorder(node_ptr tree);
 
 int main(){
   system("cls");
@@ -24,8 +26,11 @@ int main(){
   do
   {
     printf("\n1. Insert");
-    printf("\n2. Search");
-    printf("\n3. Print the elements");
+    printf("\n2. Print in inorder");
+    printf("\n3. Print in preorder");
+    printf("\n4. Print in postorder");
+    printf("\n5. Delete a node.");
+    printf("\n6. Height of the tree");
     printf("\n\nEnter '0' to quit.");
     printf("\n\nEnter choice: ");
     scanf("%d", &c);
@@ -34,91 +39,86 @@ int main(){
     {
     case 1:
       {
-        int ele;
-        printf("Enter element: ");
-        scanf("%d", &ele);
-        insert(&root, ele);
+        root = create();
         break;
       }
     case 2:
-      {
-        int ele;
-        printf("Enter element: ");
-        scanf("%d", &ele);
-        node_ptr n = search(&root, ele);
-        if(n!=NULL){
-          printf("Element found!\n");
-        }
-        else{
-          printf("Element not found\n");
-        }
-        break;
-      }
+      inorder(root);
+      break;
     case 3:
-      print_preorder(root);
+      preorder(root);
       break;
     case 4:
-      del_tree(root);
+      postorder(root);
+      break;
+    case 5:
+      printf("\nTO BE IMPLEMENTED\n");
+      // TODO: Implement delete a node
+      // delete(int data);
+      break;
+
+    case 6:
+      printf("Height of the tree is: %d", height(root));
       break;
     default:
+      printf("\nEnter a valid option\n");
       break;
     }
   }
   while(c!=0);
+  return 0;
 }
 
-node_ptr create_node(int data){
-  node_ptr nn = malloc(sizeof(node)); 
-  nn->left=NULL;
-  nn->right=NULL;
-  nn->data=data;
+node_ptr create(){
+  int value;
+  node_ptr nn = malloc(sizeof(node));
+  scanf("%d", &value);
+  nn->data=value;
+  if(value==-1) return 0;
+  printf("Enter the value for the left child of %d: ", value);
+  nn->left = create();
+  printf("Enter the value for the right child of %d: ", value);
+  nn->right = create();
   return nn;
 }
 
-void insert(node_ptr* tree, int data)
-{
-  node_ptr temp = NULL;
-  if(!(*tree)){
-    temp = create_node(data);
-    *tree=temp;
-    printf("Inserted\n");
-    return;
-  }
 
-  if(data < (*tree)->data){
-    insert(&(*tree)->left, data);
-  }else if (data > (*tree)->data){
-    insert(&(*tree)->right, data);
-  }
+void inorder(node_ptr root){
+  if(root==NULL) return;
+  inorder(root->left);
+  printf("%d ", root->data);
+  inorder(root->right);
 }
 
-node_ptr search(node_ptr* tree, int data){
-  if(!(*tree)){
-    return NULL;
-  }
-
-  if(data == (*tree)->data){
-    return *tree;
-  }else if(data < (*tree)->data){
-    search(&(*tree)->left, data);
-  }else if (data > (*tree)->data){
-    search(&(*tree)->right, data);
-  }
+void postorder(node_ptr root){
+  if(root==NULL) return;
+  postorder(root->left);
+  postorder(root->right);
+  printf("%d ", root->data);
 }
-void print_preorder(node_ptr tree)
-{
-  if (tree)
+
+void preorder(node_ptr root){
+  if(root==NULL) return;
+  printf("%d ", root->data);
+  preorder(root->left);
+  preorder(root->right);
+}
+
+void levelorder(node_ptr root){
+  if(root==NULL) return;
+  printf("%d ", root->data);
+}
+
+int height(node_ptr root){
+  if(root==NULL) return 0;
+  int lheight = height(root->left);
+  int rheight = height(root->right);
+
+  if(lheight > rheight){
+    return (lheight + 1);
+  }else
   {
-    printf("%d\n", tree->data);
-    print_preorder(tree->left);
-    print_preorder(tree->right);
+    return (rheight + 1);
   }
-}
 
-void del_tree(node_ptr tree){
-  if(tree){
-    tree->left=NULL;
-    tree->right=NULL;
-    free(tree);
-  }
 }
